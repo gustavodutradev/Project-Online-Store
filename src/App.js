bc4496ebafb77a1521caef33fef0f75c781cceb5
Alexandre <alexmanson12345@gmail.com>
2022-08-05 18:23:27 -0300

    requesito 3 incompleto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import CarrinhoDeCompra from './components/BotaoDoCarrinho';
 import Content from './components/Content';
 
 class App extends React.Component {
@@ -7,7 +8,13 @@ class App extends React.Component {
     return (
       <div>
         <h1>Front-end Online Store</h1>
-        <Content />
+        <Router>
+          <Content />
+          <CarrinhoDeCompra />
+          <Route path=>
+
+          </Route>
+        </Router>
       </div>
     );
   }
---
+++ src/components/BotaoDoCarrinho.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class CarrinhoDeCompra extends Component {
+  // state = {
+  //   name: '',
+  // }
+
+  // handleButtonClick = () => {
+  //   const button =
+  // }
+
+  render() {
+    return (
+      <Link to="/src/components/CarrinhoVazio.jsx">
+        <div data-testid="shopping-cart-empty-message">
+          <button type="submit">🛒</button>
+        </div>
+      </Link>
+    );
+  }
+}
