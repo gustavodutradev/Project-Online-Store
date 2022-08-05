3d2eec5c0cbf72dbdf4309f70d5989b9fa77a617
PedroSantana <phsantana99@gmail.com>
2022-08-05 16:21:15 -0300

    requisito 2 finalizado, pedro + gustavo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Content from './components/Content';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        #VQV
+        <h1>Front-end Online Store</h1>
+        <Content />
       </div>
     );
   }
