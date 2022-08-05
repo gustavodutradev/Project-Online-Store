ead3a06766f7ec818acbaad916e26decb964f122
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-08-05 17:47:25 -0300

    [GRUPO 2] - Requisito 5 - Pinho

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import * as api from '../services/api';
 
 class Content extends Component {
   state = {
     searchInput: '',
+    clickSearch: false,
+    searchResult: [],
   }
 
   handleChange = ({ target }) => {
@@ -11,8 +14,42 @@ class Content extends Component {
     });
   }
 
-  render() {
+  searchRequest = async () => {
     const { searchInput } = this.state;
+    const request = await api.getProductsFromCategoryAndQuery('', searchInput);
+    const { results } = await request;
+
+    let arrayOfItens = 'Nenhum produto foi encontrado';
+
+    if (results.length > 0) {
+      arrayOfItens = results.map((product) => (
+        <div key={ product.id } data-testid="product">
+          <div>
+            <img
+              src={ product.thumbnail }
+              alt={ product.title }
+            />
+          </div>
+          { product.title }
+          <br />
+          { product.price
+            .toLocaleString('pt-BR',
+              { style: 'currency',
+                currency: product.currency_id,
+                minimumFractionDigits: 2 }) }
+        </div>
+      ));
+    }
+
+    this.setState({
+      searchResult: arrayOfItens,
+      clickSearch: true,
+    });
+  }
+
+  render() {
+    // Input Handler - Checker
+    const { searchInput, searchResult, clickSearch } = this.state;
     const searchMinLimit = 0;
     const searchRule = searchInput.length > searchMinLimit;
 
@@ -24,16 +61,24 @@ class Content extends Component {
             id="home-initial-message"
             onChange={ this.handleChange }
             placeholder="Digite aqui"
+            data-testid="query-input"
           />
         </label>
-        { searchRule ? '' : (
+        <button
+          type="submit"
+          data-testid="query-button"
+          onClick={ this.searchRequest }
+        >
+          Buscar
+        </button>
+        {searchRule ? '' : (
           <p
             data-testid="home-initial-message"
           >
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
         )}
-
+        { clickSearch && searchResult }
       </div>
     );
   }
