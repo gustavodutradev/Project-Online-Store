6facce8f77b7cbe251cf94e0d41eb1d20688b363
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-08-05 16:39:57 -0300

    requisito 1 - criação da requisição da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,28 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const request = await fetch(URL);
+    const result = await request.json();
+    return result;
+  } catch (e) {
+    return e.message;
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    let URL = '';
+    if (!categoryId) {
+      URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    } else if (!query) {
+      URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+    } else {
+      URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    }
+    const request = await fetch(URL);
+    const result = await request.json();
+    return result;
+  } catch (e) {
+    return e.message;
+  }
 }
