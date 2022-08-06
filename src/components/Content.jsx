f4ad4395902543e8d7cafc4a4e89702affc3ea1e
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-08-06 15:19:09 -0300

    Requisito 6 finalizado

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,87 +1,35 @@
+// Main Imports
 import React, { Component } from 'react';
-import * as api from '../services/api';
+import PropTypes from 'prop-types';
 
 class Content extends Component {
-  state = {
-    searchInput: '',
-    clickSearch: false,
-    searchResult: [],
-  }
-
-  handleChange = ({ target }) => {
-    this.setState({
-      searchInput: target.value,
-    });
-  }
-
-  searchRequest = async () => {
-    const { searchInput } = this.state;
-    const request = await api.getProductsFromCategoryAndQuery('', searchInput);
-    const { results } = await request;
-
-    let arrayOfItens = 'Nenhum produto foi encontrado';
-
-    if (results.length > 0) {
-      arrayOfItens = results.map((product) => (
-        <div key={ product.id } data-testid="product">
-          <div>
-            <img
-              src={ product.thumbnail }
-              alt={ product.title }
-            />
-          </div>
-          { product.title }
-          <br />
-          { product.price
-            .toLocaleString('pt-BR',
-              { style: 'currency',
-                currency: product.currency_id,
-                minimumFractionDigits: 2 }) }
-        </div>
-      ));
-    }
-
-    this.setState({
-      searchResult: arrayOfItens,
-      clickSearch: true,
-    });
-  }
-
   render() {
-    // Input Handler - Checker
-    const { searchInput, searchResult, clickSearch } = this.state;
+    // Props Import
+    const { searchInput, searchResult, clickSearch } = this.props;
+
+    // Input Checker
     const searchMinLimit = 0;
     const searchRule = searchInput.length > searchMinLimit;
 
     return (
-      <div>
-        <label htmlFor="home-initial-message">
-          <input
-            type="text"
-            id="home-initial-message"
-            onChange={ this.handleChange }
-            placeholder="Digite aqui"
-            data-testid="query-input"
-          />
-        </label>
-        <button
-          type="submit"
-          data-testid="query-button"
-          onClick={ this.searchRequest }
-        >
-          Buscar
-        </button>
+      <div className="search-results">
         {searchRule ? '' : (
-          <p
-            data-testid="home-initial-message"
-          >
+          <div data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
+          </div>
         )}
-        { clickSearch && searchResult }
+        { clickSearch && (searchResult.length > 0
+          ? searchResult
+          : <div>Nenhum produto foi encontrado</div>) }
       </div>
     );
   }
 }
+
+Content.propTypes = {
+  searchInput: PropTypes.string.isRequired,
+  searchResult: PropTypes.instanceOf(Array).isRequired,
+  clickSearch: PropTypes.bool.isRequired,
+};
 
 export default Content;
