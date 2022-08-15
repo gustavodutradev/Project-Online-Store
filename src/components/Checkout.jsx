4fc704890f20d8aeb2062cf5c035252e4a761958
gustavoBarros <gustavo.dutra11@gmail.com>
2022-08-14 23:11:56 -0300

    Estilização página de Checkout em andamento

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable max-lines */
 /* eslint-disable react/jsx-max-depth */
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { FaCcVisa, FaBarcode, FaCcMastercard, FaCreditCard } from 'react-icons/fa';
 
 export default class Checkout extends Component {
   state = {
@@ -70,90 +72,106 @@ export default class Checkout extends Component {
       const { validateFields } = this.state;
       const { getCartItemQuantity, totalPrice } = this.props;
       return (
-        <div
-          className="form-container"
-        >
-          <form
-            className="checkout-form"
-          >
-            <div
-              className="buyer-data"
-            >
-              <p>Informações do Comprador</p>
-              <section>
+        <div className="checkout-container">
+          <form className="checkout-form">
+            <div className="buyer-data">
+              <div className="full-name-container">
                 <input
                   type="text"
                   placeholder="Nome Completo"
                   data-testid="checkout-fullname"
                   name="fullName"
                   onChange={ this.handleChange }
+                  className="full-name-input"
                 />
-              </section>
-              <section>
-                <input
-                  type="text"
-                  placeholder="CPF"
-                  data-testid="checkout-cpf"
-                  name="cpf"
-                  onChange={ this.handleChange }
-                />
-              </section>
-              <section>
-                <input
-                  type="email"
-                  placeholder="E-mail"
-                  data-testid="checkout-email"
-                  name="email"
-                  onChange={ this.handleChange }
-                />
-              </section>
-              <section>
-
-                <input
-                  type="text"
-                  placeholder="Telefone"
-                  data-testid="checkout-phone"
-                  name="phone"
-                  onChange={ this.handleChange }
-                />
-              </section>
-              <section>
-                <input
-                  type="text"
-                  placeholder="CEP"
-                  data-testid="checkout-cep"
-                  name="cep"
-                  onChange={ this.handleChange }
-                />
-              </section>
-              <section>
-
+              </div>
+              <div className="grid">
+                <div className="cpf-container">
+                  <input
+                    type="text"
+                    placeholder="CPF"
+                    data-testid="checkout-cpf"
+                    name="cpf"
+                    onChange={ this.handleChange }
+                    className="cpf-input"
+                  />
+                </div>
+                <div className="email-container">
+                  <input
+                    type="email"
+                    placeholder="E-mail"
+                    data-testid="checkout-email"
+                    name="email"
+                    onChange={ this.handleChange }
+                    className="email_input"
+                  />
+                </div>
+                <div className="phone-container">
+                  <input
+                    type="text"
+                    placeholder="Telefone"
+                    data-testid="checkout-phone"
+                    name="phone"
+                    onChange={ this.handleChange }
+                    className="phone-input"
+                  />
+                </div>
+                <div className="cep-container">
+                  <input
+                    type="text"
+                    placeholder="CEP"
+                    data-testid="checkout-cep"
+                    name="cep"
+                    onChange={ this.handleChange }
+                    className="cep-input"
+                  />
+                </div>
+              </div>
+              <div className="adress-container">
                 <input
                   type="text"
                   placeholder="Endereço"
                   data-testid="checkout-address"
                   name="adress"
                   onChange={ this.handleChange }
+                  className="adress-input"
                 />
-              </section>
-              <br />
-              <div>
-                <p>Método de Pagamento</p>
-                <section>
+              </div>
+            </div>
+            <div className="payment">
+              <p>Método de Pagamento</p>
+              <div className="icons">
+                <label htmlFor="boleto" className="boleto-container">
+                  <FaBarcode
+                    className="boleto"
+                  />
                   <input
                     type="radio"
                     id="boleto"
                     name="paymentType"
                     data-testid="ticket-payment"
                     onChange={ this.handleChange }
+                    className="boleto-radio"
                   />
                   Boleto
+                </label>
+                <label htmlFor="visa" className="visa-container">
+                  <FaCcVisa
+                    className="visa"
+                  />
                   <input
                     type="radio"
                     name="paymentType"
                     data-testid="visa-payment"
                     id="visa"
                     onChange={ this.handleChange }
+                    className="visa-radio"
+                  />
+                  Visa
+                </label>
+                <label htmlFor="master" className="master-container">
+                  <FaCcMastercard
+                    className="master"
                   />
                   <input
                     type="radio"
@@ -161,6 +179,13 @@ export default class Checkout extends Component {
                     data-testid="master-payment"
                     id="master"
                     onChange={ this.handleChange }
+                    className="master-radio"
+                  />
+                  MasterCard
+                </label>
+                <label htmlFor="elo" className="elo-container">
+                  <FaCreditCard
+                    className="elo"
                   />
                   <input
                     type="radio"
@@ -168,71 +193,75 @@ export default class Checkout extends Component {
                     data-testid="elo-payment"
                     id="elo"
                     onChange={ this.handleChange }
+                    className="elo-radio"
                   />
-
-                </section>
+                  Elo
+                </label>
               </div>
-              <section>
-                { validateFields && (
-                  <span
-                    data-testid="error-msg"
-                  >
-                    Campos inválidos
-                  </span>
-                )}
+            </div>
+            <div>
+              { validateFields && (
+                <span
+                  data-testid="error-msg"
+                >
+                  Campos inválidos
+                </span>
+              )}
+              <div className="btn-container">
                 <button
                   type="button"
                   data-testid="checkout-btn"
                   onClick={ this.validateFormOnClick }
+                  className="chk-btn"
                 >
                   Finalizar compra
                 </button>
-              </section>
-            </div>
-            <div
-              className="purchase-summary"
-            >
-              <p>Resumo da compra</p>
-              <section>
-                { itemsToShow.map((item) => {
-                  const { id, price, thumbnail, title } = item;
-                  return (
-                    <section key={ id }>
-                      <p>{ title }</p>
-                      <img
-                        src={ thumbnail }
-                        alt={ title }
-                      />
-                      <span>
-                        { price.toLocaleString('pt-BR',
-                          { style: 'currency',
-                            currency: 'brl',
-                            minimumFractionDigits: 2 }) }
 
-                      </span>
-                      <br />
-                      <span>
-                        Quantidade:
-                        { getCartItemQuantity(id) }
-                      </span>
-                    </section>
-                  );
-                })}
-              </section>
-              <section>
-                Total:
-                { totalPrice().toLocaleString('pt-BR',
-                  { style: 'currency',
-                    currency: 'brl',
-                    minimumFractionDigits: 2 }) }
-              </section>
+              </div>
             </div>
-            {/* <Link
+          </form>
+          <div
+            className="purchase-summary"
+          >
+            <p>Resumo da compra</p>
+            <div>
+              { itemsToShow.map((item) => {
+                const { id, price, thumbnail, title } = item;
+                return (
+                  <div key={ id }>
+                    <p>{ title }</p>
+                    <img
+                      src={ thumbnail }
+                      alt={ title }
+                    />
+                    <span>
+                      { price.toLocaleString('pt-BR',
+                        { style: 'currency',
+                          currency: 'brl',
+                          minimumFractionDigits: 2 }) }
+
+                    </span>
+                    <span>
+                      Quantidade:
+                      { getCartItemQuantity(id) }
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              Total:
+              { totalPrice().toLocaleString('pt-BR',
+                { style: 'currency',
+                  currency: 'brl',
+                  minimumFractionDigits: 2 }) }
+            </div>
+          </div>
+          {/* <Link
             to="/carrinho"
           >
             <p>Voltar para o carrinho</p>
           </Link> */}
-          </form>
         </div>
       );
     }
